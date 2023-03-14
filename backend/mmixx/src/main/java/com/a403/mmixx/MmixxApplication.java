7eabfab33a765302fd127f7d60a41a99673407ba
ealswjd <mjjr0424@naver.com>
2023-03-14 10:19:19 +0900

    init backend 초기세팅 230314

---
+++ backend/mmixx/src/main/java/com/a403/mmixx/MmixxApplication.java
@@ -0,0 +1,13 @@
+package com.a403.mmixx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmixxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmixxApplication.class, args);
+	}
+
+}
