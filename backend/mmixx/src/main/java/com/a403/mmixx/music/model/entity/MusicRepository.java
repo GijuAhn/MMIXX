ceb40cc22c3133e30e09a03d45fd3be23bb5629e
leejiyeon <sky75013@naver.com>
2023-03-15 17:57:51 +0900

    feat: music api update, delete, getlist

--- backend/mmixx/src/main/java/com/a403/mmixx/music/model/entity/MusicRepository.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/entity/MusicRepository.java
@@ -2,5 +2,6 @@ package com.a403.mmixx.music.model.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MusicRepository extends JpaRepository<Music, Long> {
+public interface MusicRepository extends JpaRepository<Music, Integer>, MusicRepositoryCustom {
+	// Page<Music> findAll(Pageable pageable);
 }
--- backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MusicService.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MusicService.java
@@ -1,8 +1,12 @@
 package com.a403.mmixx.music.model.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.a403.mmixx.music.model.dto.MusicCondition;
 import com.a403.mmixx.music.model.dto.MusicDetailResponseDto;
+import com.a403.mmixx.music.model.dto.MusicListResponseDto;
 import com.a403.mmixx.music.model.entity.Music;
 import com.a403.mmixx.music.model.entity.MusicRepository;
 
@@ -13,9 +17,38 @@ import lombok.RequiredArgsConstructor;
 public class MusicService {
 	private final MusicRepository musicRepository;
 
-	public MusicDetailResponseDto getMusic(Long seq) {
+	public Page<MusicListResponseDto> getMusicList(Pageable pageable) {
+		return musicRepository.findAll(pageable).map(MusicListResponseDto::new);
+	}
+
+	public Page<MusicListResponseDto> getMusicListByCondition(MusicCondition condition, Pageable pageable) {
+		return musicRepository.getMusicListByCondition(condition, pageable);
+	}
+
+	public MusicDetailResponseDto getMusic(Integer seq) {
 		Music music = musicRepository.findById(seq)
 			.orElseThrow(() -> new IllegalArgumentException("해당 음악은 없습니다. 방 ID: " + seq)); // error code: 500
 		return new MusicDetailResponseDto(music);
+	}
+
+	public Music updateMusic(Integer seq, String musicName) {
+		Music music = musicRepository.findById(seq).orElse(null);
+		if (music != null) {
+			if (musicName == null || musicName.length() == 0) { // TODO: 수정 필요
+				System.out.println("...musicName = null...!!");
+			} else {
+				System.out.println("...musicName is not null... : " + musicName);
+			}
+			music.updateMusic(musicName);
+		}
+		return music;
+	}
+
+	public Music deleteMusic(Integer seq) {
+		Music music = musicRepository.findById(seq).orElse(null);
+		if (music != null) {
+			musicRepository.deleteById(seq);
+		}
+		return music;
 	}
 }
