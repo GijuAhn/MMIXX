0c620e33495f2503bbe472da7551fe9218675a1a
SuhyungK <dook4966@gmail.com>
2023-03-15 14:52:05 +0900

    feat: 플레이리스트 전체 조회 api 230315

---
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/entity/Playlist.java
@@ -0,0 +1,27 @@
+package com.a403.mmixx.playlist.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@DynamicInsert
+public class Playlist {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long playlistSeq;
+	@Column(nullable = false)
+	private Long userSeq;
+	@Column(length = 100, nullable = false)
+	private String playlistName;
+	@Column(nullable = false)
+	private Boolean isPrivate;
+}
