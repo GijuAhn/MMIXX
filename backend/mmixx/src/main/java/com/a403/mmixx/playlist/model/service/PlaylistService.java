65a61ad73bc509be82435f4d1f8f69405e040513
SuhyungK <dook4966@gmail.com>
2023-03-15 14:52:05 +0900

    feat: 플레이리스트 전체 조회 api 230315

---
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/service/PlaylistService.java
@@ -0,0 +1,14 @@
+package com.a403.mmixx.playlist.model.service;
+
+import com.a403.mmixx.playlist.model.entity.PlaylistRepository;
+import lombok.RequiredArgsConstructor;
+
+@org.springframework.stereotype.Service
+@RequiredArgsConstructor
+public class PlaylistService {
+	private final PlaylistRepository musicRepository;
+
+	public static void main(String[] args) {
+
+	}
+}
