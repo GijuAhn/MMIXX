5969a2fd87e961df25e26dee6e31f7de7d8b148e
leejiyeon <sky75013@naver.com>
2023-03-15 12:17:40 +0900

    feat: music api 음악상세조회 추가 230315

---
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/entity/MusicRepository.java
@@ -0,0 +1,6 @@
+package com.a403.mmixx.music.model.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MusicRepository extends JpaRepository<Music, Long> {
+}
