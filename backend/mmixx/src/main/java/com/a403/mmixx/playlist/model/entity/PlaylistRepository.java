ce598f0cce2ac92c4c4317a890f6e45aecaa536c
SuhyungK <dook4966@gmail.com>
2023-03-15 17:32:07 +0900

    #S08P22A403-97 feat: 플레이리스트 전체 조회 api 230315

--- backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/entity/PlaylistRepository.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/entity/PlaylistRepository.java
@@ -3,4 +3,6 @@ package com.a403.mmixx.playlist.model.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+
+
 }
