ee716322080939c606b9f9b9f9fb8b902f4155ca
leejiyeon <sky75013@naver.com>
2023-03-16 11:05:15 +0900

    fix: updateMusic 안되는 거 수정 완료 230316

--- backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MusicService.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MusicService.java
@@ -1,5 +1,7 @@
 package com.a403.mmixx.music.model.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.a403.mmixx.music.model.dto.MusicCondition;
 import com.a403.mmixx.music.model.dto.MusicDetailResponseDto;
 import com.a403.mmixx.music.model.dto.MusicListResponseDto;
+import com.a403.mmixx.music.model.dto.MusicUpdateRequestDto;
 import com.a403.mmixx.music.model.entity.Music;
 import com.a403.mmixx.music.model.entity.MusicRepository;
 
@@ -31,19 +34,16 @@ public class MusicService {
 		return new MusicDetailResponseDto(music);
 	}
 
-	public Music updateMusic(Integer seq, String musicName) {
+	@Transactional
+	public Music updateMusic(Integer seq, MusicUpdateRequestDto requestDto) {
 		Music music = musicRepository.findById(seq).orElse(null);
 		if (music != null) {
-			if (musicName == null || musicName.length() == 0) { // TODO: 수정 필요
-				System.out.println("...musicName = null...!!");
-			} else {
-				System.out.println("...musicName is not null... : " + musicName);
-			}
-			music.updateMusic(musicName);
+			music.updateMusic(requestDto);
 		}
 		return music;
 	}
 
+	@Transactional
 	public Music deleteMusic(Integer seq) {
 		Music music = musicRepository.findById(seq).orElse(null);
 		if (music != null) {
