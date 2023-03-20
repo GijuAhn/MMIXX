9cb3e29f8bf240100d91f91a14b7a1515cc8612b
SuhyungK <dook4966@gmail.com>
2023-03-20 12:38:32 +0900

    init: styled-components 라이브러리 -> emotion 라이브러리로 변경

---
+++ frontend/src/router/Router.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const index = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default index;
