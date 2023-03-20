5c08a882fe454210b1ef390be4068f971c7abd91
SuhyungK <dook4966@gmail.com>
2023-03-20 17:41:24 +0900

    design: Global Style 적용, NavBar, Main 기본 레이아웃 설정

---
+++ frontend/src/pages/Playlist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Playlist = () => {
+  return (
+    <div>
+      <h2>Playlist</h2>
+    </div>
+  );
+};
+
+export default Playlist;
--- frontend/src/styles/theme.js
+++
@@ -1,7 +0,0 @@
-const theme = {
-  colors: {
-    primary: 'hotpink'
-  }
-}
-
-export default theme;
