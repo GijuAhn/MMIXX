a26b0b0b02791b71b5168b44d41b4806432661ba
Giju Ahn <wn7257@gmail.com>
2023-03-21 18:11:30 +0900

    feat: extract metadata, upload S3, get Music,CoverImage URL

--- backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MusicService.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MusicService.java
@@ -1,32 +1,23 @@
 package com.a403.mmixx.music.model.service;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.a403.mmixx.music.model.dto.*;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
-import com.a403.mmixx.music.model.dto.MusicCondition;
-import com.a403.mmixx.music.model.dto.MusicDetailResponseDto;
-import com.a403.mmixx.music.model.dto.MusicListResponseDto;
-import com.a403.mmixx.music.model.dto.MusicUpdateRequestDto;
 import com.a403.mmixx.music.model.entity.Music;
 import com.a403.mmixx.music.model.entity.MusicRepository;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -46,7 +37,7 @@ public class MusicService {
 
 	public MusicDetailResponseDto getMusic(Integer seq) {
 		Music music = musicRepository.findById(seq)
-			.orElseThrow(() -> new IllegalArgumentException("해당 음악은 없습니다. 방 ID: " + seq)); // error code: 500
+				.orElseThrow(() -> new IllegalArgumentException("해당 음악은 없습니다. 방 ID: " + seq)); // error code: 500
 		return new MusicDetailResponseDto(music);
 	}
 
@@ -67,9 +58,69 @@ public class MusicService {
 		}
 		return music;
 	}
-	public List<String> registMusic(List<MultipartFile> multipartFiles) {
-		// TODO (파일 확장자, ... 메타 데이터 추출 및 DB에 저장)
-		// TODO 한번에 업로드? -> 몇개까지 제한?
-		return awsS3Service.uploadMusic(multipartFiles);
+
+	public List<Music> registMusic(List<MultipartFile> multipartFiles) {
+//		TODO: DB에 저장하는 로직 추가
+		return getID3v2Metadata(multipartFiles);
 	}
+
+
+	//	Extract Metadata from ID3v2 format
+	public List<Music> getID3v2Metadata(List<MultipartFile> multipartFiles) {
+
+		List<String> musicUrlList;
+		List<String> coverImageList;
+		List<Music> musicContainerList = new ArrayList<>();
+
+		musicUrlList = awsS3Service.uploadMusicToS3(multipartFiles);
+		coverImageList = awsS3Service.uploadCoverImageToS3(multipartFiles);
+
+		multipartFiles.forEach(file -> {
+			Music musicContainer = null;
+			try {
+				musicContainer = new Music();
+
+				String musicName = "";
+				String musicUrl = "";
+				String coverImage = "";
+				int musicLength = 0;
+				String musicianName = "";
+				String albumName = "";
+
+				Mp3File mp3file = new Mp3File(file.getOriginalFilename());
+				if (mp3file.hasId3v2Tag()) {
+					ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+					musicName = id3v2Tag.getTitle();
+					musicianName = id3v2Tag.getArtist();
+					albumName = id3v2Tag.getAlbum();
+					musicLength = Math.toIntExact(mp3file.getLengthInSeconds());
+					byte[] coverImageData = id3v2Tag.getAlbumImage();
+
+					if (coverImageData != null) {
+						System.out.println("Have album image data, length: " + coverImageData.length + " bytes");
+						System.out.println("Album image mime type: " + id3v2Tag.getAlbumImageMimeType());
+					}
+				}
+
+				musicContainer.setMusicName(musicName);
+				musicContainer.setMusicUrl(musicUrl);
+				musicContainer.setCoverImage(coverImage);
+				musicContainer.setMusicLength(musicLength);
+				musicContainer.setMusicianName(musicianName);
+				musicContainer.setAlbumName(albumName);
+
+			} catch (InvalidDataException | UnsupportedTagException | IOException e) {
+				e.printStackTrace();
+			}
+			musicContainerList.add(musicContainer);
+		});
+
+		for (int i = 0; i < musicContainerList.size(); i++) {
+			musicContainerList.get(i).setMusicUrl(musicUrlList.get(i));
+			musicContainerList.get(i).setCoverImage(coverImageList.get(i));
+		}
+
+		return musicContainerList;
+	}
+
 }
