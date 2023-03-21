e9e7c3eb87033a6badce382de710eede8c7a0b17
SuhyungK <dook4966@gmail.com>
2023-03-21 14:29:53 +0900

    fix: PlaylistResponseDto 삭제

--- backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/entity/PlaylistRepository.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/entity/PlaylistRepository.java
@@ -1,9 +1,6 @@
 package com.a403.mmixx.playlist.model.entity;
 
-import com.a403.mmixx.playlist.model.dto.PlaylistListResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 //    List<Playlist> findAll();
