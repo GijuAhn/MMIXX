0f61824c50035c19da16821f74c9eadfbd61af75
SuhyungK <dook4966@gmail.com>
2023-03-21 16:50:12 +0900

    feat: playbar 컴포넌트 화면에 따라 resize

--- frontend/src/atom/atom.js
+++ frontend/src/atom/atom.js
@@ -1,6 +1,11 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
-export const navBarWidth = atom({
-  key: 'NavBarWidth',
-  default: 300,
+export const defaultWidth = atom({
+  key: 'defaultWidth',
+  default: window.innerWidth - 300,
+})
+
+export const changePlaybarWidth = selector({
+  key: 'changePlaybarWidth',
+  get: ({get}) => { return get(defaultWidth) }
 })
