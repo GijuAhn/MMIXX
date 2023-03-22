241c9919b723df9c4a465086988dbb783825b37b
Giju Ahn <wn7257@gmail.com>
2023-03-22 12:56:23 +0900

    chore: clean project

--- backend/mmixx/src/main/java/com/a403/mmixx/config/SecurityConfig.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/config/SecurityConfig.java
@@ -30,24 +30,25 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/","/css/**","/images/**","/js/**"
-                        ,"/h2-console/**", "/profile").permitAll()
-//                .anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/") // 로그아웃 이후 연결 페이지
-                .invalidateHttpSession(true)
-                .and()
-                .oauth2Login() // OAuth2기반의 로그인
-                .loginPage("/loginForm")
-                .defaultSuccessUrl("/") // 로그인 성공
-                .failureUrl("/loginForm") // 로그인 실패
-                .userInfoEndpoint() // 로그인 성공 후 사용자정보를 가져온다
-                .userService(customOAuth2UserService); // 사용자 정보를 customOAuth2UserService로 처리
-        // .and()
-        // Access Token이 만료된 경우 ContextHolder에서 권한을 찾아 해당 요청에 대해서만 접근을 허용해줘야 하기 때문에 Filter를 완전히 제거할 수는 없음 (addFilterBefore 제거하지 말것!)
-        // .addFilterBefore(UsernamePasswordAuthenticationFilter.class);
+                .antMatchers("/**").permitAll();
+//                .antMatchers("/","/css/**","/images/**","/js/**"
+//                        ,"/h2-console/**", "/profile").permitAll();
+////                .anyRequest().authenticated()
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/") // 로그아웃 이후 연결 페이지
+//                .invalidateHttpSession(true)
+//                .and()
+//                .oauth2Login() // OAuth2기반의 로그인
+//                .loginPage("/loginForm")
+//                .defaultSuccessUrl("/") // 로그인 성공
+//                .failureUrl("/loginForm") // 로그인 실패
+//                .userInfoEndpoint() // 로그인 성공 후 사용자정보를 가져온다
+//                .userService(customOAuth2UserService); // 사용자 정보를 customOAuth2UserService로 처리
+//        // .and()
+//        // Access Token이 만료된 경우 ContextHolder에서 권한을 찾아 해당 요청에 대해서만 접근을 허용해줘야 하기 때문에 Filter를 완전히 제거할 수는 없음 (addFilterBefore 제거하지 말것!)
+//        // .addFilterBefore(UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
