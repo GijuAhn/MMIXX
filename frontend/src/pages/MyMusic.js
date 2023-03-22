15c7a7dc70d081dbc4d83053cee1917db25a223e
leejiyeon <sky75013@naver.com>
2023-03-22 17:58:39 +0900

    fix: My Music 페이지 musiclist 컴포넌트 수정

--- frontend/src/pages/MyMusic.js
+++ frontend/src/pages/MyMusic.js
@@ -1,9 +1,11 @@
 import { Content } from "components/Content";
+import MusicList from "components/mymusic/MusicList";
 
 const MyMusic = () => {
   return (
     <Content>
       <h2>MyMusic</h2>
+      <MusicList></MusicList>
     </Content>
   );
 };
