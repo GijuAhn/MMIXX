ba1fd391cc78adc82eefcc295e9fe1595ece0651
leejiyeon <sky75013@naver.com>
2023-03-22 11:03:04 +0900

    chore: my music 페이지 musiclist 컴포넌트 수정중 230322

--- frontend/src/components/mymusic/MusicList.jsx
+++ frontend/src/components/mymusic/MusicList.jsx
@@ -1,14 +1,25 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const MusicList = () => {
-  if (window.location.pathname === "/mix" || window.location.pathname === "/") {
-    return null;
-  }
+  // if (window.location.pathname === "/mix" || window.location.pathname === "/") {
+  //   return null;
+  // }
+  const [loading, setLoading] = useState(true);
+  const [musicList, setMusicList] = useState([]);
 
+  useEffect(() =>
+    //fetch  ... .then response
+    //.then((response) => response.json())
+    //.then((json) => setMusicList(json))
+    {}, []); // []이면 첫 렌더링될 때 한번만
   return (
-    <Body>
-      <h2>PlayBar</h2>
-    </Body>
+    <div>
+      {loading ? <strong>Loading...</strong> : null}
+      <Body>
+        <h2>PlayBar</h2>
+      </Body>
+    </div>
   );
 };
 
