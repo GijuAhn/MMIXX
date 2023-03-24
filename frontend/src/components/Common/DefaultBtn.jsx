4fe3e162cd1bf84a48e0c9f17acf03c2fe835732
SuhyungK <dook4966@gmail.com>
2023-03-24 16:04:42 +0900

    design: DefaultBtn 컴포넌트 제작

--- frontend/src/components/Common/DefaultBtn.jsx
+++ frontend/src/components/Common/DefaultBtn.jsx
@@ -1,8 +1,17 @@
 import styled from "styled-components"
 
 const DefaultBtn = styled.button`
-  & :hover {
-    transform: scale(1.2, 1.2);
+  background-color: ${({theme}) => theme.palette.secondary};
+  color: ${({theme}) => theme.palette.dark};
+  padding: 10px 20px;
+  border-radius: 27px;
+  border: none;
+  font-size: 14px;
+  transition: all 0.1s ease-in-out;
+
+  &: hover {
+    transform: scale(1.1);
+    filter: brightness(1.05);
   }
 `
 
