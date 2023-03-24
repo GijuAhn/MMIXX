6d294137c8d223dafaf9f7d03339d876c5ddd6bc
Giju Ahn <wn7257@gmail.com>
2023-03-24 14:29:08 +0900

    fix: multipartfile convert to inputstream 일회성 휘발문제

--- backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MP3MetadataService.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MP3MetadataService.java
@@ -1,5 +1,6 @@
 package com.a403.mmixx.music.model.service;
 
+import com.a403.mmixx.music.model.entity.Music;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.mp3.Mp3Parser;
@@ -7,23 +8,65 @@ import org.apache.tika.sax.BodyContentHandler;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class MP3MetadataService {
 
-    public Map<String, String> extractMetadata(MultipartFile file) throws Exception {
+    public static List<Music> extractMetadataFromMultipartFileList(List<MultipartFile> multipartFiles) throws Exception {
+
+        List<Music> musicContainerList = new ArrayList<>();
+
+        for (MultipartFile multipartFile : multipartFiles) {
+            Music musicContainer = new Music();
+            Map<String, String> metadataMap = extractMetadata(multipartFile);
+            musicContainer.setMusicName(metadataMap.get("musicName"));
+            musicContainer.setMusicLength((int) Math.ceil(Double.valueOf(metadataMap.get("musicLength")) * 1000));
+            musicContainer.setMusicianName(metadataMap.get("musicianName"));
+            musicContainer.setAlbumName(metadataMap.get("albumName"));
+            musicContainerList.add(musicContainer);
+        }
+
+        //  print all metadata (filtered data check)
+        for (Music music : musicContainerList) {
+            System.out.println("<musicName> : " + music.getMusicName());
+            System.out.println("<musicLength> : " + music.getMusicLength() + "(milliseconds)");
+            System.out.println("<musicianName> : " + music.getMusicianName());
+            System.out.println("<albumName> : " + music.getAlbumName());
+        }
+
+        return musicContainerList;
+    }
+
+    public static Map<String, String> extractMetadata(MultipartFile file) throws Exception {
+        // Convert multipart file to MP3 file
+        File mp3File = Files.createTempFile("temp", ".mp3").toFile();
+
+        //  print file's location (path) for debugging
+        System.out.println(mp3File.getAbsolutePath());
+
+
+        file.transferTo(mp3File);
+
         Map<String, String> metadataMap = new HashMap<>();
 
         // Create a Tika parser and parse the MP3 file's metadata
         BodyContentHandler handler = new BodyContentHandler();
         Metadata metadata = new Metadata();
-        InputStream stream = file.getInputStream();
-        ParseContext parseContext = new ParseContext();
 
+        // File to InputStream
+        InputStream stream = new FileInputStream(mp3File);
+        ParseContext parseContext = new ParseContext();
         Mp3Parser parser = new Mp3Parser();
         parser.parse(stream, handler, metadata, parseContext);
 
@@ -33,12 +76,14 @@ public class MP3MetadataService {
         metadataMap.put("musicianName", metadata.get("xmpDM:artist"));
         metadataMap.put("albumName", metadata.get("xmpDM:album"));
 
-        //  print all metadata
+        //  print all metadata (raw data check)
         String[] metadataNames = metadata.names();
         for(String name : metadataNames){
             System.out.println(name + ": " + metadata.get(name));
         }
 
+        // Delete temporary MP3 file
+        mp3File.delete();
         return metadataMap;
     }
 
