faca1de27225d61e5dc24a7e83dcab3f3100cca2
suhyungK <dook4966@gmail.com>
2023-03-26 18:25:27 +0900

    fix: 플레이리스트 추가 페이지로 넘어올 때 텍스트 선택 되는 부분 수정
    Former-commit-id: 7d45d2f3bf2f99431669389a288d9d2df4cbd5f5

--- frontend/src/pages/PlaylistCreate.js
+++ frontend/src/pages/PlaylistCreate.js
@@ -1,101 +1,97 @@
-  import styled, { css } from "styled-components"
-  import React from 'react'
-  import { useRecoilValue } from "recoil";
-  import AlbumIcon from '@mui/icons-material/Album';
+import { useRef, useEffect } from 'react'
+import styled, { css } from "styled-components"
+import { useRecoilValue } from "recoil"
+import AlbumIcon from '@mui/icons-material/Album'
 
-  import { Wrapper, Header, DefaultBtn } from "components/Common"
-  import { tempMusic } from "atom/atom";
+import { Wrapper, Header, DefaultBtn } from "components/Common"
+import { tempMusic } from "atom/atom"
 
-  const PlaylistCreate = () => {
-    const playlist = useRecoilValue(tempMusic)
-    const inputRef = React.useRef(null);
+const PlaylistCreate = () => {
+  const playlist = useRecoilValue(tempMusic)
+  const inputRef = useRef(null)
 
-    console.log(inputRef.current)
-    React.useEffect(() => {
-      if (inputRef !== null) {
-        console.log(inputRef)
-        // inputRef.current.select();
-      }
-    }, [inputRef])
+  useEffect(() => {
+    inputRef.current.select()
+    inputRef.current.focus()
+  })
 
-    console.log(playlist)
-    return (
-      <StyleWrapper url="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWN8ZW58MHx8MHx8&w=1000&q=80">
-        <Header 
-          title="New Playlist"
-          desc="플레이리스트 만들기"
-        />
-        <InputContent>
-          <DefaultCover coverImage={playlist.coverImage}>
-            <AlbumIcon colo="white" fontSize="large"/>
-          </DefaultCover>
-          <InputRight>
-            <InputTitle>
-              <input type="text" autoFocus defaultValue="#플레이리스트 제목" onFocus={() => inputRef.current.select()}></input>
-            </InputTitle>
-            <DefaultBtn>
-              곡 추가
-            </DefaultBtn>
-          </InputRight>
-        </InputContent>
-      </StyleWrapper>
-    );
-  };
+  return (
+    <StyleWrapper url="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWN8ZW58MHx8MHx8&w=1000&q=80">
+      <Header 
+        title="New Playlist"
+        desc="플레이리스트 만들기"
+      />
+      <InputContent>
+        <DefaultCover coverImage={playlist.coverImage}>
+          <AlbumIcon colo="white" fontSize="large"/>
+        </DefaultCover>
+        <InputRight>
+          <InputTitle>
+            <input type="text" ref={inputRef} defaultValue="#플레이리스트 제목"></input>
+          </InputTitle>
+          <DefaultBtn>
+            곡 추가
+          </DefaultBtn>
+        </InputRight>
+      </InputContent>
+    </StyleWrapper>
+  );
+};
 
-  const StyleWrapper = styled(Wrapper)`
-    ${({theme, url}) => css`
-      background-image: linear-gradient(to bottom left, rgba(0, 0, 0, 0.7), ${theme.palette.dark} 70%), url(${url});
-      background-size: cover;
-    `}
-  `
+const StyleWrapper = styled(Wrapper)`
+  ${({theme, url}) => css`
+    background-image: linear-gradient(to bottom left, rgba(0, 0, 0, 0.7), ${theme.palette.dark} 70%), url(${url});
+    background-size: cover;
+  `}
+`
 
-  const InputContent = styled.div`
-    height: 350px;
-    width: 1100px;
-    overflow: hidden;
-    display: grid;
-    padding: 0px 10px;
-    grid-template-columns: 300px 700px;
-    gap: 30px;
-    justify-content: start;
-  `
+const InputContent = styled.div`
+  height: 350px;
+  width: 1100px;
+  overflow: hidden;
+  display: grid;
+  padding: 0px 10px;
+  grid-template-columns: 300px 700px;
+  gap: 30px;
+  justify-content: start;
+`
 
-  const DefaultCover = styled.div`
-    width: 300px;
-    height: 300px;
-    background-color: ${({theme}) => theme.palette.dark};
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  ` 
+const DefaultCover = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: ${({theme}) => theme.palette.dark};
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+` 
 
-  const InputRight = styled.div`
-    width: 700px;
-    flex-direction: column;
-    align-items: start;
+const InputRight = styled.div`
+  width: 700px;
+  flex-direction: column;
+  align-items: start;
 
-    * {
-      margin: 10px 0;
-    }
-  `
+  * {
+    margin: 10px 0;
+  }
+`
 
-  const InputTitle = styled.div`
+const InputTitle = styled.div`
+  color: #fff;
+  font-size: 50px;
+  font-weight: bold;
+  justify-content: start;
+
+  & input {
     color: #fff;
+    background-color: transparent;
     font-size: 50px;
-    font-weight: bold;
-    justify-content: start;
-
-    & input {
-      color: #fff;
-      background-color: transparent;
-      font-size: 50px;
-      font-weight: 800;
-      border: none;
-      border-bottom: 1px solid white;
-      
-      :focus {
-        outline: none;
-    }
-  `
-  export default PlaylistCreate;
+    font-weight: 800;
+    border: none;
+    border-bottom: 1px solid white;
+    
+    :focus {
+      outline: none;
+  }
+`
+export default PlaylistCreate;
