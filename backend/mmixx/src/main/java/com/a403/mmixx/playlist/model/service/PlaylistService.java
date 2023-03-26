29ac619718a470ffd545db4999ba61cfb9b79f16
suhyungK <dook4966@gmail.com>
2023-03-26 14:09:51 +0900

    fix: 플레이리스트 api 전체 조회, 개별 조회 수정
    Former-commit-id: 08e9d5f3016efc55f72cf4810b5d2bd2d3d5799e

---
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/service/PlaylistService.java
@@ -0,0 +1,65 @@
+package com.a403.mmixx.playlist.model.service;
+
+//import com.a403.mmixx.playlist.model.dto.PlaylistMusicListResponseDto;
+import com.a403.mmixx.playlist.model.dto.PlaylistDto;
+import com.a403.mmixx.playlist.model.dto.PlaylistMusicDto;
+import com.a403.mmixx.playlist.model.dto.PlaylistMusicResponseDto;
+import com.a403.mmixx.playlist.model.entity.Playlist;
+import com.a403.mmixx.playlist.model.entity.PlaylistMusic;
+import com.a403.mmixx.playlist.model.entity.PlaylistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PlaylistService {
+
+    @Autowired
+    private PlaylistRepository playlistRepository;
+
+    /**
+     * 전체 플레이리스트 조회
+     */
+    public List<PlaylistDto> getPlaylist() {
+        return playlistRepository.findByIsPrivateFalse().stream()
+                .map(PlaylistDto::new).collect(Collectors.toList());
+    }
+
+    /**
+     * 플레이리스트에 속한 노래 목록 조회
+     */
+    public PlaylistMusicResponseDto getPlaylistMusic(int seq) {
+        Playlist playlist = playlistRepository.findById(seq)
+                .orElseThrow(() -> new RuntimeException("Invalid playlistSeq"));
+        List<PlaylistMusic> playlistMusic = playlistRepository.findByPlaylistSeq(seq);
+
+        List<PlaylistMusicDto> playlistMusicDtos = playlistMusic.stream()
+                .map(pm -> new PlaylistMusicDto(pm))
+                .collect(Collectors.toList());
+
+        return PlaylistMusicResponseDto.builder()
+                .playlistSeq(seq)
+                .playlistMusic(playlistMusicDtos)
+                .build();
+    }
+
+//    /**
+//     * 플레이리스트 삭제
+//     */
+//    public void deletePlaylist(int id) {
+//        Optional<Playlist> playlist = playlistRepository.findById(id);
+//
+//        if (playlist != null) {
+//            playlistRepository.deleteById(id);
+//        }
+//    }
+//
+//    public String getCoverImage(int seq) {
+//        return "";
+//    }
+}
