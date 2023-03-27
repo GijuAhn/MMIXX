28a0941166e8a6cbf7b16b73f89933befc3e49f3
castleis <tjddms7973@gmail.com>
2023-03-27 13:11:22 +0900

    feat:add mix carousel
    Former-commit-id: 390b7dda260d0436a513c1e98d00a33f0cb69c78

--- frontend/src/pages/Mix.js
+++ frontend/src/pages/Mix.js
@@ -1,4 +1,12 @@
 import { Wrapper, Header } from "components/Common";
+// import { Carousel } from "components/Mix/carousel";
+
+import { Carousel } from 'react-carousel3';
+
+const style = {
+  width: 297,
+  height: 296,
+};
 
 const Mix = () => {
   return (
@@ -7,6 +15,34 @@ const Mix = () => {
         title="MIX"
         desc="음악 믹스하기"  
       />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          background: 'linear-gradient(to bottom, #16235e 0%, #020223 100%)',
+        }}
+      >
+      <Carousel height={460} width={980} yOrigin={42} yRadius={48} autoPlay={true}>
+        <div key={1} style={style}>
+          <img alt="" src="./image/1.png" />
+        </div>
+        <div key={2} style={style}>
+          <img alt="" src="./image/2.png" />
+        </div>
+        <div key={3} style={style}>
+          <img alt="" src="./image/3.png" />
+        </div>
+        <div key={4} style={style}>
+          <img alt="" src="./image/4.png" />
+        </div>
+        <div key={5} style={style}>
+          <img alt="" src="./image/5.png" />
+        </div>
+        <div key={6} style={style}>
+          <img alt="" src="./image/6.png" />
+        </div>
+      </Carousel>
+      </div>
     </Wrapper>
   );
 };
