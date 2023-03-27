fcbd23e081b476f0f1f2efec582d577f09ba6aa7
suhyungK <dook4966@gmail.com>
2023-03-27 10:54:36 +0900

    feat: 플레이리스트 프론트 api 추가
    Former-commit-id: b0291e5a0e4d1230ab56e7ac044381bb0df0c7a2

--- frontend/src/api/playlist.js
+++ frontend/src/api/playlist.js
@@ -1,7 +1,14 @@
 import base from './base'
+import axios from 'axios'
 
 export const getPlaylist = async() => {
   const data = await base.get()
 
   return base;
+}
+
+export const getPlaylistDetail = async( params ) => {
+  const data = await axios(params)
+
+  return data
 }
