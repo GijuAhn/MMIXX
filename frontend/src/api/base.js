0f2de0a03b81999469e22621c701b167c7944b83
SuhyungK <dook4966@gmail.com>
2023-03-28 09:34:14 +0900

    feat: front 플레이리스트 api 추가
    Former-commit-id: de7657f99b6e7b644c57af8c82f53a1152e02666

--- frontend/src/api/base.js
+++ frontend/src/api/base.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const instance =  axios.create({
-  baseURL: process.env.REACT_APP_TEST,
-  headers: {
-    'Authorization': `Bearer ${localStorage.getItem('token')}`
-  }
+  baseURL: process.env.REACT_APP_BASEURL,
+  // headers: {
+  //   'Authorization': `Bearer ${localStorage.getItem('token')}`
+  // }
 })
 
 export default instance;
