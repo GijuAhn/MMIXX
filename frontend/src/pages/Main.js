43da96566dc2e2a53a20436e8ef972bc6e1963c7
SuhyungK <dook4966@gmail.com>
2023-03-28 10:34:40 +0900

    desgin: main 페이지 aos 라이브러리 추가
    Former-commit-id: eafa02a7346743b8c455b6920c35f55d164993c0

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,5 @@
-// import styled from 'styled-components'
 import { useEffect } from 'react';
+import styled from 'styled-components'
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -12,18 +12,44 @@ const Main = () => {
   })
 
   return (
-    <Wrapper>
-      <div style={{ border: '1px solid red'}} data-aos="slide-up" data-aos-duration="3000" data-aos-anchor-placement="bottom-bottom">
-        asdfasdf
-      </div>
-    </Wrapper>
+    <StyleWrapper>
+      <First data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="bottom-bottom">
+        MUSIC<br />
+        MIXING<br />
+        FOR WHOLE NEW EXPERIENCE<br />
+      </First>
+      <First data-aos="slide-up" data-aos-duration="1000" data-aos-anchor-placement="bottom-bottom">
+        MUSIC MAKE 어쩌꾸
+      </First>
+      <First data-aos="slide-up" data-aos-duration="1000" data-aos-anchor-placement="bottom-bottom">
+        MUSIC MAKE 어쩌꾸
+      </First>
+      <First data-aos="slide-up" data-aos-duration="1000" data-aos-anchor-placement="bottom-bottom">
+        MUSIC MAKE 어쩌꾸
+      </First>
+      <First data-aos="slide-up" data-aos-duration="1000" data-aos-anchor-placement="bottom-bottom">
+        MUSIC MAKE 어쩌꾸
+      </First>
+      
+      {/* <div style={{ border: '1px solid red'}} >
+        MUSIC 
+      </div> */}
+    </StyleWrapper>
   );
 };
 
-// const MainBody = styled.div`
-//   width: 100%;
-//   height: 100vh;
-//   background-color: ${({ theme }) => theme.palette.darkAlt }
-// `
+const StyleWrapper = styled(Wrapper)`
+  flex-direction: column;
+
+  > div {
+    height: 500px;
+  }
+`
+
+const First = styled.div`
+  border: 1px solid red;
+  font-size: 50px;
+  font-weight: 800;
+`
 
 export default Main;
