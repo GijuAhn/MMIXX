94e6ec3a35b6053e3b18e05ff9d932dc6c69d9c6
문승우 <city1616@naver.com>
2023-03-29 14:37:41 +0900

    feat: user_seq로 회원 모든 음악 조회 구현 230329
    Former-commit-id: 05d14b1294421e83975b7c8dfd09fb7f9dd8a78d

--- backend/mmixx/src/main/java/com/a403/mmixx/music/model/entity/MusicRepository.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/entity/MusicRepository.java
@@ -1,7 +1,9 @@
 package com.a403.mmixx.music.model.entity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MusicRepository extends JpaRepository<Music, Integer>, MusicRepositoryCustom {
-	// Page<Music> findAll(Pageable pageable);
+	 Page<Music> findByUserSeq(Integer user_seq, Pageable pageable);
 }
