d4ea2c8dfa29d9d69766a38e67f97a457fbc2581
suhyungK <dook4966@gmail.com>
2023-03-29 14:56:31 +0900

    feat: PlayBar 재생 버튼 클릭 -> 노래 재생
    Former-commit-id: 5d8e033ac74b8289a3a1969584aae5738d120180

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,20 +1,26 @@
 import { Wrapper } from 'components/Common';
-import styled from 'styled-components'
-import Retro from 'assets/music/NewJeans-Retro.mp3';
+// import styled from 'styled-components'
+// import Retro from 'assets/music/NewJeans-Retro.mp3';
+import Remix from 'assets/music/NewJeans-Future Funk Remix.mp3';
 
 const Test = () => {
   // const audioElement = new Audio("https://ipaudio3.club/wp-content/uploads/HARFREE/Dale%2001%20-%20Sorcerer's%20Stone/Chapter%2001%20-%20The%20Boy%20Who%20Lived.mp3?_=1")
-  const audioElement = new Audio(Retro)
+  const audioElement = new Audio(Remix)
   // const audioElement = new Audio("https://www.youtube.com/watch?v=jT0Lh-N3TSg")
   
   const playAudio = () => {
     audioElement.play();
   }
 
+  setTimeout(() => {
+    console.log(audioElement.duration)
+    console.log(audioElement.currentTime)
+  }, 1000)
+
   return (
     <Wrapper>
       <button style={{ color: 'red'}} onClick={() => playAudio()}>
-        {Retro}
+        {Remix}
       </button>
     </Wrapper>
   )
