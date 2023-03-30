1fdbdf8904580d1bb8b1da6f1d102058b02dcd58
문승우 <city1616@naver.com>
2023-03-30 15:38:09 +0900

    feat: favorite 등록 api 230330
    Former-commit-id: a1b209f480bdc83a0f09591dd64c6e6c07834f8f

--- backend/mmixx/src/main/java/com/a403/mmixx/playlist/controller/PlaylistController.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/controller/PlaylistController.java
@@ -2,14 +2,18 @@
 
 package com.a403.mmixx.playlist.controller;
 
+import com.a403.mmixx.playlist.model.dto.FavoriteRequestDto;
 import com.a403.mmixx.playlist.model.dto.PlaylistMusicRequestDto;
 import com.a403.mmixx.playlist.model.service.PlaylistService;
 import com.amazonaws.Response;
+
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
  import org.springframework.web.bind.annotation.*;
 
+@Api(tags = {"플레이리스트", "api", "Playlist"})
 @RestController
 @RequestMapping("/playlist")
 public class PlaylistController {
@@ -35,7 +39,12 @@ public class PlaylistController {
     public void deletePlaylist(@PathVariable("playlistSeq") int seq) {
         playlistService.deletePlaylist(seq);
     }
-//
+    
+    @ApiOperation(value = "즐겨찾기 등록")
+    @PostMapping("/favorite")
+    public ResponseEntity<?> insertFavorite(@RequestBody FavoriteRequestDto favoriteRequestDto) {
+    	return ResponseEntity.ok(playlistService.insertFavorite(favoriteRequestDto));
+    }
 //    @ApiOperation(value = "플레이리스트 커버 이미지")
 //    @GetMapping("/{playlistSeq}/1")
 //    public String getCoverImage(@PathVariable("playlistSeq") int seq){
