d2fa840d13ec8cfb60bec7c8ce2a4163ea06414f
suhyungK <dook4966@gmail.com>
2023-03-30 17:38:45 +0900

    design: NavBar 디자인 재수정
    Former-commit-id: 1a69bc9ccf9fffe9ee646525dc205d5811caa07f

--- frontend/src/components/PlayBar/VolumeControl.jsx
+++ frontend/src/components/PlayBar/VolumeControl.jsx
@@ -10,26 +10,26 @@ const VolumeControl = ({ audioElement }) => {
     <>
       <VolumeDownRounded />
       <Slider
-            aria-label="Volume"
-            defaultValue={30}
-            sx={{
-              // color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
-              '& .MuiSlider-track': {
-                border: 'none',
-              },
-              '& .MuiSlider-thumb': {
-                width: 24,
-                height: 24,
-                backgroundColor: '#fff',
-                '&:before': {
-                  boxShadow: '0 4px 8px rgba(0,0,0,0.4)',
-                },
-                '&:hover, &.Mui-focusVisible, &.Mui-active': {
-                  boxShadow: 'none',
-                },
-              },
-            }}
-          />
+        aria-label="Volume"
+        defaultValue={30}
+        sx={{
+          // color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
+          '& .MuiSlider-track': {
+            border: 'none',
+          },
+          '& .MuiSlider-thumb': {
+            width: 24,
+            height: 24,
+            backgroundColor: '#fff',
+            '&:before': {
+              boxShadow: '0 4px 8px rgba(0,0,0,0.4)',
+            },
+            '&:hover, &.Mui-focusVisible, &.Mui-active': {
+              boxShadow: 'none',
+            },
+          },
+        }}
+      />
       <VolumeUpRounded />
     </>
 
