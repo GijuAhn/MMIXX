11d41a790eec5b5cb3e827529e3443862ef44c2c
문승우 <city1616@naver.com>
2023-03-30 15:55:55 +0900

    feat: 즐겨찾기 삭제 API 230330
    Former-commit-id: a4b126d3e35f5913dc3f75e3c76754b547adf3ea

--- backend/mmixx/src/main/java/com/a403/mmixx/playlist/controller/PlaylistController.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/controller/PlaylistController.java
@@ -45,6 +45,14 @@ public class PlaylistController {
     public ResponseEntity<?> insertFavorite(@RequestBody FavoriteRequestDto favoriteRequestDto) {
     	return ResponseEntity.ok(playlistService.insertFavorite(favoriteRequestDto));
     }
+    
+    @ApiOperation(value = "즐겨찾기 삭제")
+    @DeleteMapping("/favorite/{user_seq}/{playlist_seq}")
+    public ResponseEntity<?> deleteFavorite(@PathVariable int user_seq, @PathVariable int playlist_seq) {
+    	System.out.println("user_seq : " + user_seq + " playlist_seq : " + playlist_seq);
+    	String response = playlistService.deleteFavorite(user_seq, playlist_seq);
+    	return ResponseEntity.ok(response);
+    }
 //    @ApiOperation(value = "플레이리스트 커버 이미지")
 //    @GetMapping("/{playlistSeq}/1")
 //    public String getCoverImage(@PathVariable("playlistSeq") int seq){
--- backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/entity/FavoriteRepository.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/entity/FavoriteRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
 	Favorite findByUserSeqAndPlaylistSeq(int user_seq, int playlist_seq);
+	void deleteByUserSeqAndPlaylistSeq(int user_seq, int playlist_seq);
 }
