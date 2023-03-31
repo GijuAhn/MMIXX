c89380e2dcd30eb5b8131b5d8d4d322e4755279f
leejiyeon <sky75013@naver.com>
2023-03-31 14:12:25 +0900

    chore: mymusic api url 230331
    Former-commit-id: 34d04a19330d0ff4c51d15e58d326a393844ad95

--- frontend/src/api/mymusic.js
+++ frontend/src/api/mymusic.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: "http://j8a403.p.ssafy.io/api",
-  // baseURL: 'http://localhost:5555/api', // 로컬 테스트
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("auth")}`,
-  },
+  // baseURL: "http://j8a403.p.ssafy.io/api",
+  baseURL: "http://localhost:5555/api", // 로컬 테스트
+  // headers: {
+  //   Authorization: `Bearer ${localStorage.getItem("auth")}`,
+  // },
 });
 
 const musicUrl = `/music`;
