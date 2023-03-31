a4a1080ebbb666a424361d1bb03a707297f31535
ealswjd <mjjr0424@naver.com>
2023-03-31 14:35:53 +0900

    feat: logout 230331
    Former-commit-id: a02bc62f53643a07e44fd63f461a915d3a9a2a70

--- frontend/src/api/base.js
+++ frontend/src/api/base.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const instance =  axios.create({
-  // baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
   // baseURL: 'http://j8a403.p.ssafy.io/api',
-  baseURL: 'http://localhost:5555/api', // 로컬 테스트
+  // baseURL: 'http://localhost:5555/api', // 로컬 테스트
   headers: {
     'Authorization': `Bearer ${localStorage.getItem('auth')}`
   }
@@ -12,7 +12,7 @@ const instance =  axios.create({
 // instance.defaults.headers.common['Authorization'] = 'token';
 
 export const handleLogout = async () => {
-  return await instance.post({
+  return await instance({
     url: `/logout`
   })
 }
