11bd8cce65c3bdbb262a02839e16b0f13ae066be
문승우 <city1616@naver.com>
2023-03-31 13:11:08 +0900

    refactor: Music에서 Preset 분리 230331
    Former-commit-id: 2a2176f31ed51dc1514a144f782064d1c983f05f

--- backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MusicService.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/service/MusicService.java
@@ -11,10 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -22,8 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.a403.mmixx.music.model.entity.Music;
 import com.a403.mmixx.music.model.entity.MusicRepository;
-import com.a403.mmixx.music.model.entity.Preset;
-import com.a403.mmixx.music.model.entity.PresetRepository;
+import com.a403.mmixx.preset.model.entity.Preset;
+import com.a403.mmixx.preset.model.entity.PresetRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -283,7 +281,4 @@ public class MusicService {
 		return responseDto;
 	}
 	
-	public List<Preset> findAllPreset() {
-		return presetRepository.findAll();
-	}
 }
