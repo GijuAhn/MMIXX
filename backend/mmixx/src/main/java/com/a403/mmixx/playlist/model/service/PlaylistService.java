82b9cffbc4d48f5af0a65a372bc67f8509b57521
Giju Ahn <wn7257@gmail.com>
2023-03-31 09:58:57 +0900

    feat: playlist created and add musics
    Former-commit-id: 8b74205f551dbcca1f9336d8739cd2caadac2633

--- backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/service/PlaylistService.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/playlist/model/service/PlaylistService.java
@@ -1,22 +1,24 @@
 package com.a403.mmixx.playlist.model.service;
 
 //import com.a403.mmixx.playlist.model.dto.PlaylistMusicListResponseDto;
+import com.a403.mmixx.music.model.entity.MusicRepository;
 import com.a403.mmixx.playlist.model.dto.*;
 import com.a403.mmixx.playlist.model.entity.Playlist;
 import com.a403.mmixx.playlist.model.entity.PlaylistMusic;
 import com.a403.mmixx.playlist.model.entity.PlaylistMusicRepository;
 import com.a403.mmixx.playlist.model.entity.PlaylistRepository;
-import com.a403.mmixx.user.model.entity.User;
 import com.a403.mmixx.user.model.entity.UserRepository;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import jdk.security.jarsigner.JarSigner;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -31,47 +33,81 @@ public class PlaylistService {
     private PlaylistRepository playlistRepository;
     @Autowired
     private PlaylistMusicRepository playlistMusicRepository;
+
+    @Autowired
+    private MusicRepository musicRepository;
     private UserRepository userRepository;
 
-    /**
-     * DTO로 빈 플레이리스트 생성.
-     * 최초 생성시 playlist만 생성하고, playlistMusic은 updatePlaylistMusic에서 생성
-     */
-    public void createEmptyPlaylist(PlaylistMusicRequestDtoForCreateAndModify requestDto) {
-        User user = userRepository.findById(requestDto.getUserSeq())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid userSeq"));
-
-        Playlist playlist = new Playlist();
-
-        playlist.setUser(user);
-        playlist.setPlaylistName(requestDto.getPlaylistName());
-        playlist.setIsPrivate(requestDto.isPrivate());
-
-        playlistRepository.save(playlist);
+    //  JSON from Frontend - Example
+    /*
+    {
+    "playlist_name":"플리이름",
+    "is_private":true,
+    "user_seq":6,
+    "playlist_music":[
+        {
+            "music_seq":1,
+            "sequence":1
+        },
+        {
+            "music_seq":2,
+            "sequence":2
+        }
+    ]
     }
-
+     */
 
     /**
-     * JSON 객체를 받아 빈 플레이리스트 생성.
-     * 최초 생성시 playlist만 생성하고, playlistMusic은 updatePlaylistMusic에서 생성
+     * 빈 플레이리스트 생성 + 생성된 플레이리스트에 곡 리스트 추가한 후 DB 저장까지
+     * @param requestDto
+     * @param userSeq
      */
     @Transactional
-    public Playlist createEmptyPlaylistByJSON(JsonObject jsonObjectForCreatePlaylist, int userSeq) {
+    public void createPlaylist(@RequestBody PlaylistDto requestDto, int userSeq) {
 
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!userSeq = " + userSeq);
-        log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!userSeq= " + userSeq);
+        // check requestDto got from frontend
+        log.info("requestDto: " + requestDto);
+        log.info("playlistName: " + requestDto.getPlaylistName());
+        log.info("isPrivate: " + requestDto.getIsPrivate());
+        log.info("userSeq: " + requestDto.getUserSeq());
+        log.info("playlistMusic: " + requestDto.getPlaylistMusic());
 
-        Playlist playlist = new Playlist();
 
-        String playlistName = jsonObjectForCreatePlaylist.get("playlist_name").getAsString();
-        Boolean isPrivate = jsonObjectForCreatePlaylist.get("is_private").getAsBoolean();
+        //  PART1: empty playlist create
+        Playlist newPlaylist = new Playlist();
+        newPlaylist.setPlaylistName(requestDto.getPlaylistName());
+        newPlaylist.setIsPrivate(requestDto.getIsPrivate());
+//java.lang.IllegalArgumentException: Can not set java.lang.Integer field com.a403.mmixx.user.model.entity.User.userSeq to java.lang.Integer
+//        newPlaylist.setUserSeq(userSeq);
+        playlistRepository.save(newPlaylist);
+        log.info("[1/6] !!empty playlist created successfully!!");
 
-        playlist.setUser(userRepository.findById(userSeq).orElseThrow(() -> new IllegalArgumentException("Invalid userSeq")));
-        playlist.setPlaylistName(playlistName);
-        playlist.setIsPrivate(isPrivate);
+        // get last increment playlist_seq
+        Integer playlistSeq = newPlaylist.getPlaylistSeq();
+        log.info("playlistSeq: " + playlistSeq);
+        log.info("[2/6]!!playlistSeq(id) Retrieved successfully!!");
 
-        playlistRepository.save(playlist);
-        return playlist;
+        //  PART2: playlist_music create
+        List<PlaylistMusicDto> playlistMusicList = requestDto.getPlaylistMusic();
+
+        for (int i = 0; i < playlistMusicList.size(); i++) {
+            PlaylistMusic newPlaylistMusic = new PlaylistMusic();
+
+//"org.springframework.orm.jpa.JpaSystemException: Error accessing field [private java.lang.Integer com.a403.mmixx.music.model.entity.Music.musicSeq] by reflection for persistent property
+//            newPlaylistMusic.setPlaylistSeq(playlistSeq);
+//            newPlaylistMusic.setSequence(playlistMusicList.get(i).getSequence());
+//            newPlaylistMusic.setMusicSeq(playlistMusicList.get(i).getMusicSeq());
+
+            newPlaylistMusic.setPlaylist(playlistRepository.findById(playlistSeq).orElse(null));
+            log.info("[3/6] !!playlistMusic object from JSON setting done!!");
+            newPlaylistMusic.setMusic(musicRepository.findById(playlistMusicList.get(i).getMusicSeq()).orElse(null));
+            log.info("[4/6] !!music_seq setting done!!");
+            newPlaylistMusic.setSequence(playlistMusicList.get(i).getSequence());
+            log.info("[5/6] !!sequence setting done!!");
+            playlistMusicRepository.save(newPlaylistMusic);
+            log.info("[6/6] !!playlistMusic objects (list) saved successfully!!");
+        }
+
     }
 
 
@@ -120,11 +156,10 @@ public class PlaylistService {
     }
 
 
-    /**
-     * 플레이리스트 수정(업데이트), 곡 추가
-     */
-    public void updatePlaylistMusic(PlaylistMusicRequestDtoForCreateAndModify requestDto) {
-    }
+
+
+
+
 
 
 
