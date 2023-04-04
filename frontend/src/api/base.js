8d77c0fe1f0058af1cf6f9857aed3330bc569452
SuhyungK <dook4966@gmail.com>
2023-04-04 10:46:09 +0900

    chore: 전체 높이 조정
    Former-commit-id: f4a380c28801aa928eeede29e18fa8cc46cf1df2

--- frontend/src/api/base.js
+++ frontend/src/api/base.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const instance =  axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: 'http://j8a403.p.ssafy.io/api',
-  // baseURL: 'http://localhost:5555/api', // 로컬 테스트
+  // baseURL: 'http://j8a403.p.ssafy.io/api',
+  baseURL: 'http://localhost:5555/api', // 로컬 테스트
   // headers: {
   //   'Authorization': `Bearer ${localStorage.getItem('auth')}`
   // }
