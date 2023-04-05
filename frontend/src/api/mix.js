0911fcf6a3c36df923743562baf379258a6f799c
SuhyungK <dook4966@gmail.com>
2023-04-05 14:55:08 +0900

    feat: 재생 진행중
    Former-commit-id: 55545b14536b102fc22120a7dc66d51afd70708d

--- frontend/src/api/mix.js
+++ frontend/src/api/mix.js
@@ -2,8 +2,8 @@ import instance from './base'
 
 export const musicMix = async ( music_seq, preset_seq ) => {
   return await instance.post('/music/mix', {
-    headers: {
-      "Content-Type": "multipart/json",
-    }
+    // headers: {
+    //   "Content-Type": "multipart/json",
+    // }
   })
 }
