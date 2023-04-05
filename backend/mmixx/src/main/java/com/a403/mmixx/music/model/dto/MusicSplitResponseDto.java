b97641c6004542aa3b7b41dcba787c819032986a
문승우 <city1616@naver.com>
2023-04-05 11:03:44 +0900

    fix: 보컬 제거 responseDto 수정 230405
    Former-commit-id: 69257f39f0e00c5a88ad334b66d6b2eee7d8b723

--- backend/mmixx/src/main/java/com/a403/mmixx/music/model/dto/MusicSplitResponseDto.java
+++ backend/mmixx/src/main/java/com/a403/mmixx/music/model/dto/MusicSplitResponseDto.java
@@ -1,12 +1,36 @@
 package com.a403.mmixx.music.model.dto;
 
+import com.a403.mmixx.music.model.entity.Music;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class MusicSplitResponseDto {
-	private String music_inst_url;
+	private Integer userSeq;
+	private String musicName;
+	private String musicUrl;
+	private String coverImage;
+	private Integer musicLength;
+	private String musicianName;
+	private String albumName;
+	private String genre;
+	private Integer mixed;
+	private Integer inst;
+	private Integer presetSeq;
 	
-	public MusicSplitResponseDto(String music_inst_url) {
-		this.music_inst_url = music_inst_url;
+	public MusicSplitResponseDto(Music entity) {
+		this.userSeq = entity.getUser().getUserSeq();
+		this.musicName = entity.getMusicName();
+		this.musicUrl = entity.getMusicUrl();
+		this.coverImage = entity.getCoverImage();
+		this.musicLength = entity.getMusicLength();
+		this.musicianName = entity.getMusicianName();
+		this.albumName = entity.getAlbumName();
+		this.genre = entity.getGenre();
+		this.mixed = null;
+		this.inst = entity.getInst().getMusicSeq();
+		this.presetSeq = entity.getPresetSeq();
 	}
 }
