e1057b30684d5a0182c3b12e840550281cd71730
leejiyeon <sky75013@naver.com>
2023-04-05 13:37:28 +0900

    feat: 곡 업로드/배경음 추출 성공 시 업로드된 곡 보여주기, count 리로드 230405
    Former-commit-id: 05b6998c580aea85608e948de3007c86847dcfe6

--- frontend/src/pages/MyMusic.js
+++ frontend/src/pages/MyMusic.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import { Wrapper, Header } from "components/Common";
-import { MusicSearchBar, MusicUploadBtn, MusicList } from "components/mymusic";
+import { MusicSearchBar, MusicUploadBtn, MusicList, CustomTable } from "components/mymusic";
 import { CustomSelect } from "components/mymusic";
 import { filterOptions, orderOptions } from "components/mymusic/options";
 import styled from "styled-components";
+
+import { useRecoilValue } from "recoil";
+import { _show_new, _new_music_list } from "atom/mymusic";
 
 const MyMusic = () => {
   const [query, setQuery] = useState("");
   const [filter, setFilter] = useState("");
   const [order, setOrder] = useState("");
   const [searchText, setSearchText] = useState("");
+
+  // const [showNew, setShowNew] = useState(false);
+  // const [newMusicList, setNewMusicList] = useState([]);
+  const showNew = useRecoilValue(_show_new);
+  const newMusicList = useRecoilValue(_new_music_list);
 
   // // [Test] 곡 선택하기 (radio)
   // const [selectedMusic, setSelectedMusic] = useState({
@@ -39,6 +47,33 @@ const MyMusic = () => {
           <CustomSelect options={orderOptions} selectKind='정렬' setSelect={setOrder} />
         </SelectSection>
       </Div>
+
+      {showNew ? <CustomTable musicList={newMusicList} isNew={true} /> : null}
+      {/* <CustomTable
+        musicList={[
+          {
+            musicSeq: 0,
+            coverImage: null,
+            mixed: null,
+            edited: null,
+            musicName: "곡 제목",
+            musicianName: "수지",
+            albumName: "앨범 이름",
+            musicLength: 35000,
+          },
+          {
+            musicSeq: 999999,
+            coverImage: null,
+            mixed: null,
+            edited: null,
+            musicName: "곡 제목9999",
+            musicianName: "뉴진스",
+            albumName: "앨범 이름9999",
+            musicLength: 35000,
+          },
+        ]}
+        isNew={true}
+      /> */}
 
       <MusicList filter={filter} order={order} query={query} setSearchText={setSearchText} />
 
