f752fb0ac519a04d2ee636cf633dcfcf146dfb03
suhyungK <dook4966@gmail.com>
2023-04-06 10:58:17 +0900

    feat: 재생/정지 추가
    Former-commit-id: 600cf079f4e4b2a0222c627eadcc58713619d9d0

--- frontend/src/components/PlayBar/PlaySlider.jsx
+++ frontend/src/components/PlayBar/PlaySlider.jsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components'
 import { useState, useEffect, useRef } from 'react'
 import Slider from '@mui/material/Slider';
 import { usePlayControl } from 'hooks/usePlayControl';
@@ -5,6 +6,11 @@ import { usePlayControl } from 'hooks/usePlayControl';
 const PlaySlider = () => {
   const { audioElement, isPlaying, nowMusic } = usePlayControl()
   const [ position, setPosition ] = useState(0)
+  const [ duration, setDuration ] = useState(0)
+  const [ time, setTime ] = useState({
+    start: '0:00',
+    end: '-:--'
+  })
   const sliderRef = useRef(null)
 
   const formatDuration = (value) => {
@@ -12,6 +18,28 @@ const PlaySlider = () => {
     const secondLeft = value - minute * 60
     return `${minute} : ${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`
   }
+
+  audioElement.onloadedmetadata = function() {
+    // duration in seconds
+    const durationInSeconds = Math.floor(audioElement.duration);
+    const currentInSeconds = Math.floor(audioElement.currentTime);
+
+    // calculate minutes and seconds
+    const minutes = Math.floor(durationInSeconds / 60);
+    const seconds = durationInSeconds % 60;
+
+    const minutes2 = Math.floor(currentInSeconds / 60);
+    const seconds2 = currentInSeconds % 60;
+
+    // format the display string
+    const displayString = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    const currentString = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+
+    setTime({
+      start: '0:00',
+      end: displayString
+    })
+  };
 
   setInterval(() => {
     console.log(parseInt(audioElement.currentTime))
@@ -22,47 +50,66 @@ const PlaySlider = () => {
   }, 1000)
 
   useEffect(() => {
-    console.log(nowMusic)
+    console.log(nowMusic.duration, '-', audioElement.duration)
     audioElement.src = nowMusic.musicUrl
   }, [])
 
   return (
-    <Slider
-      aria-label="time-indicator"
-      size="small"
-      ref={sliderRef}
-      value={position}
-      // value={0}
-      max={nowMusic.musicLength}
-      onChange={(_, value) => setPosition(value)}
-      sx={{
-        // color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
-        height: 4,
-        '& .MuiSlider-thumb': {
-          width: 8,
-          height: 8,
-          transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
-          '&:before': {
-            boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
+    <div style={{ position: 'relative'}}>
+      <Slider
+        aria-label="time-indicator"
+        size="small"
+        ref={sliderRef}
+        value={position}
+        // value={0}
+        max={nowMusic.musicLength}
+        onChange={(_, value) => setPosition(value)}
+        sx={{
+          // color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
+          height: 4,
+          '& .MuiSlider-thumb': {
+            width: 8,
+            height: 8,
+            transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
+            '&:before': {
+              boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
+            },
+            '&:hover, &.Mui-focusVisible': {
+              // boxShadow: `0px 0px 0px 8px ${
+              //   theme.palette.mode === 'dark'
+              //     ? 'rgb(255 255 255 / 16%)'
+              //     : 'rgb(0 0 0 / 16%)'
+              // }`,
+            },
+            '&.Mui-active': {
+              width: 20,
+              height: 20,
+            },
           },
-          '&:hover, &.Mui-focusVisible': {
-            // boxShadow: `0px 0px 0px 8px ${
-            //   theme.palette.mode === 'dark'
-            //     ? 'rgb(255 255 255 / 16%)'
-            //     : 'rgb(0 0 0 / 16%)'
-            // }`,
+          '& .MuiSlider-rail': {
+            opacity: 0.28,
           },
-          '&.Mui-active': {
-            width: 20,
-            height: 20,
-          },
-        },
-        '& .MuiSlider-rail': {
-          opacity: 0.28,
-        },
-      }}
-    />
+        }}
+      />
+      <TimeInfo>
+        <span>{time.start}</span>
+        <span>{time.end}</span>
+      </TimeInfo>
+    </div>
   )
 }
+
+const TimeInfo = styled.div`
+  position: absolute;
+  // border: 1px solid pink;
+  justify-content: space-between;
+  top: 20px;
+
+  span {
+    font-size: 12px;
+    font-weight: 300;
+    color: gray;
+  }
+`
 
 export default PlaySlider;
