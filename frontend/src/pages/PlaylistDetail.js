3c471e48fb5b2ecbcdd1dfbcb09201bf8d4193bd
leejiyeon <sky75013@naver.com>
2023-04-06 13:08:00 +0900

    feat: 성공 실패 toast로 알리기 230406
    Former-commit-id: 104eac68d6dd98926a49ef7ffed1138defadca0b

--- frontend/src/pages/PlaylistDetail.js
+++ frontend/src/pages/PlaylistDetail.js
@@ -1,66 +1,61 @@
-import { useState, useEffect, useRef } from "react"
-import styled, { css } from "styled-components"
-import { useNavigate, useParams } from 'react-router-dom'
-import AlbumIcon from '@mui/icons-material/Album'
-import { Switch } from '@mui/material'
-import PlayCircleFilledRoundedIcon from '@mui/icons-material/PlayCircleFilledRounded';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { useRecoilValue } from "recoil"
-import StopCircleRoundedIcon from '@mui/icons-material/StopCircleRounded';
+import { useState, useEffect, useRef } from "react";
+import styled, { css } from "styled-components";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import AlbumIcon from "@mui/icons-material/Album";
+import { Switch } from "@mui/material";
+import PlayCircleFilledRoundedIcon from "@mui/icons-material/PlayCircleFilledRounded";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useRecoilValue } from "recoil";
+import StopCircleRoundedIcon from "@mui/icons-material/StopCircleRounded";
 
-import { Wrapper, Header } from "components/Common"
-import { getPlaylistDetail, deletePlaylist, getPlaylistInfo, addFavoritePlaylist, deleteFavoritePlaylist } from "api/playlist"
-import { CustomTable } from "components/mymusic"
-import { usePlayControl } from "hooks/usePlayControl"
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import { userInfo } from 'atom/atom';
-import { getUser } from "api/user"
+import { Wrapper, Header } from "components/Common";
+import { getPlaylistDetail, deletePlaylist, getPlaylistInfo, addFavoritePlaylist, deleteFavoritePlaylist } from "api/playlist";
+import { CustomTable } from "components/mymusic";
+import { usePlayControl } from "hooks/usePlayControl";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { userInfo } from "atom/atom";
+import { getUser } from "api/user";
+import CustomToast from "components/mymusic/CustomToast";
 
 const PlaylistDetail = () => {
-  const { playlistSeq } = useParams()
-  const [ playlistMusic, setPlayListMusic ] = useState([]);
-  const [ coverImage, setCoverImage ] = useState(null)
-  const [ playlistInfo, setPlaylistInfo ] = useState({
-      playlistName: '',
-      isPrivate: true,
-      userSeq: -1
-  })
+  const { playlistSeq } = useParams();
+  const [playlistMusic, setPlayListMusic] = useState([]);
+  const [coverImage, setCoverImage] = useState(null);
+  const [playlistInfo, setPlaylistInfo] = useState({
+    playlistName: "",
+    isPrivate: true,
+    userSeq: -1,
+  });
 
-  const [ playlistUserInfo, setPlaylistUserInfo ] = useState({
-    profileImageUrl: '',
-    userName: '',
-  })
-  const { 
-    audioElement, 
-    nowMusic, 
-    queue, 
-    isPlaying, 
-    setIsPlaying, 
-    createNowPlaylist, 
-    createNowMusic 
-  } = usePlayControl(playlistSeq)
-  
-  // user 
-  const atomUser = useRecoilValue(userInfo)
+  const [playlistUserInfo, setPlaylistUserInfo] = useState({
+    profileImageUrl: "",
+    userName: "",
+  });
+  const { audioElement, nowMusic, queue, isPlaying, setIsPlaying, createNowPlaylist, createNowMusic } = usePlayControl(playlistSeq);
+
+  // user
+  const atomUser = useRecoilValue(userInfo);
 
   // 공개 여부 체크
   const [isChecked, setIsChecked] = useState(false);
 
-  useEffect(() => {
-  }, [nowMusic])
+  const { state } = useLocation();
+  const [toastSuccess, setToastSuccess] = useState(state && state.success);
+
+  useEffect(() => {}, [nowMusic]);
   /**
    * 플레이리스트 재생
    */
   const handlePlaying = async () => {
     if (queue && queue === playlistMusic) {
-      console.log(' same! ')
-      audioElement.paused ? audioElement.play() : audioElement.pause()
+      console.log(" same! ");
+      audioElement.paused ? audioElement.play() : audioElement.pause();
     } else {
-      const res = await createNowPlaylist(playlistMusic)
-      createNowMusic(res)
+      const res = await createNowPlaylist(playlistMusic);
+      createNowMusic(res);
     }
-  }
+  };
 
   // 즐겨찾기
   const [isFavorite, setIsFavorite] = useState(false);
@@ -69,112 +64,86 @@ const PlaylistDetail = () => {
     if (!isFavorite) {
       addFavoritePlaylist({
         userSeq: atomUser.userSeq,
-        playlistSeq: playlistSeq
-      }).then(res => {
+        playlistSeq: playlistSeq,
+      }).then((res) => {
         setIsFavorite(true);
-      })
+      });
     } else {
-      deleteFavoritePlaylist(atomUser.userSeq, playlistSeq).then(res => {
+      deleteFavoritePlaylist(atomUser.userSeq, playlistSeq).then((res) => {
         setIsFavorite(false);
-      })
+      });
     }
-
-  }
+  };
 
   useEffect(() => {
     // 플레이리스트 음악 목록 가져오기
     getPlaylistDetail(playlistSeq)
-      .then(res => {
-        setPlayListMusic(res.data)
-        setCoverImage(res.data[0].coverImage)
+      .then((res) => {
+        setPlayListMusic(res.data);
+        setCoverImage(res.data[0].coverImage);
       })
-      .catch(err => console.log(err))
-    
+      .catch((err) => console.log(err));
+
     // 플레이리스트 정보(제목, 공개여부, 즐겨찾기 여부 등) 가져오기
     getPlaylistInfo(playlistSeq, atomUser.userSeq)
-      .then(res => {
+      .then((res) => {
         // setPlaylistTitle(res.data.playlistName)
-        setPlaylistInfo(res.data)
-        setIsChecked(res.data.isPrivate)
-        setIsFavorite(res.data.isFavorite)
-        return res.data.userSeq
+        setPlaylistInfo(res.data);
+        setIsChecked(res.data.isPrivate);
+        setIsFavorite(res.data.isFavorite);
+        return res.data.userSeq;
       })
-      .then(res => {
-        getUser(res)
-          .then(res => setPlaylistUserInfo(res.data))
-        console.log("000 ",res.data.isFavorite)
+      .then((res) => {
+        getUser(res).then((res) => setPlaylistUserInfo(res.data));
+        // console.log("000 ",res.data.isFavorite)
       })
-      .catch(err => console.log(err))
-    
+      .catch((err) => console.log(err));
   }, [playlistSeq]);
 
   return (
     <StyleWrapper url={coverImage}>
-      <Header 
-        title="플레이리스트 상세 보기"
-        desc=""
-        fontSize="24px"
-      />
+      <Header title='플레이리스트 상세 보기' desc='' fontSize='24px' />
+      {toastSuccess ? <CustomToast res='success' text={state.msg} toggle={setToastSuccess} width={state.width} /> : null}
       <InfoContent>
-        <PlaylistCover coverImage={coverImage}>
-          {!coverImage &&
-            <AlbumIcon color="white" fontSize="large"/>
-          }
-        </PlaylistCover>
+        <PlaylistCover coverImage={coverImage}>{!coverImage && <AlbumIcon color='white' fontSize='large' />}</PlaylistCover>
         <RightContent>
-            {isFavorite ?
-              <StyleFavoriteIcon onClick={heartClick } />
-              :
-              <StyleFavoriteBorderIcon onClick={heartClick } />
-            }
+          {isFavorite ? <StyleFavoriteIcon onClick={heartClick} /> : <StyleFavoriteBorderIcon onClick={heartClick} />}
           <Top>
             <PlaylistTitle>
               <p>{playlistInfo.playlistName}</p>
             </PlaylistTitle>
             <PlaylistUser>
-              <img src={playlistUserInfo.profileImageUrl} alt="만든 사람"/>
+              <img src={playlistUserInfo.profileImageUrl} alt='만든 사람' />
               <span>{playlistUserInfo.userName}</span>
             </PlaylistUser>
-            {playlistUserInfo.userSeq === atomUser.userSeq &&
+            {playlistUserInfo.userSeq === atomUser.userSeq && (
               <PrivateToggle>
                 비공개여부
-                <Switch checked={isChecked } />
+                <Switch checked={isChecked} />
               </PrivateToggle>
-            }
+            )}
           </Top>
           <Bottom>
             {/* 재생하기 */}
-            {isPlaying && queue.playlistSeq === playlistSeq? 
-              <StopCircleRoundedIcon 
-                sx={{ fontSize: '40px'}}
-                onClick={handlePlaying}
-              />
-            :
-              <StylePlayCircleFilledRoundedIcon 
-                sx={{ fontSize: '40px'}}
-                onClick={handlePlaying}
-                disabled={playlistMusic.length === 0}
-              />
-            }
-            {playlistUserInfo.userSeq === atomUser.userSeq &&
-              <MoreIconDiv 
-                playlistMusic={playlistMusic} 
-                playlistSeq={playlistSeq}
-              />
-            }
+            {isPlaying && queue.playlistSeq === playlistSeq ? (
+              <StopCircleRoundedIcon sx={{ fontSize: "40px" }} onClick={handlePlaying} />
+            ) : (
+              <StylePlayCircleFilledRoundedIcon sx={{ fontSize: "40px" }} onClick={handlePlaying} disabled={playlistMusic.length === 0} />
+            )}
+            {playlistUserInfo.userSeq === atomUser.userSeq && <MoreIconDiv playlistMusic={playlistMusic} playlistSeq={playlistSeq} />}
           </Bottom>
         </RightContent>
       </InfoContent>
 
-      <CustomTable playlistSeq={playlistSeq} musicList={ playlistMusic }/>        
+      <CustomTable playlistSeq={playlistSeq} musicList={playlistMusic} />
     </StyleWrapper>
   );
 };
 
 const MoreIconDiv = ({ playlistMusic, playlistSeq }) => {
-  const wrapperRef = useRef(null)
-  const navigate = useNavigate()
-  const [ isOpen, setIsOpen ] = useState(false)
+  const wrapperRef = useRef(null);
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   // 플레이리스트 삭제 시작
   const useConfirm = (message = null, onConfirm, onCancel) => {
@@ -184,7 +153,7 @@ const MoreIconDiv = ({ playlistMusic, playlistSeq }) => {
     if (onCancel && typeof onCancel !== "function") {
       return;
     }
-  
+
     const confirmAction = () => {
       if (window.confirm(message)) {
         onConfirm();
@@ -192,28 +161,23 @@ const MoreIconDiv = ({ playlistMusic, playlistSeq }) => {
         onCancel();
       }
     };
-  
+
     return confirmAction;
   };
   const deleteConfirm = () => {
-    deletePlaylist(playlistSeq)
-      .then(res => {
-        // alert('삭제되었습니다.')
-        navigate('/playlist')
-      })
-  }
+    deletePlaylist(playlistSeq).then((res) => {
+      // alert('삭제되었습니다.')
+      navigate("/playlist");
+    });
+  };
   const cancelConfirm = () => console.log("취소했습니다.");
-  const confirmDelete = useConfirm(
-    "삭제하시겠습니까?",
-    deleteConfirm,
-    cancelConfirm
-  );
+  const confirmDelete = useConfirm("삭제하시겠습니까?", deleteConfirm, cancelConfirm);
   // 플레이리스트 삭제 끝
 
   const handleToggle = () => {
-    setIsOpen((pre) => !pre)
+    setIsOpen((pre) => !pre);
     // console.log(isOpen)
-  }
+  };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -232,42 +196,38 @@ const MoreIconDiv = ({ playlistMusic, playlistSeq }) => {
   return (
     <MoreDiv ref={wrapperRef}>
       <SelectSection>
-        <MoreVertIcon 
-          fontSize="small" 
-          onClick={handleToggle}
-        />
-        {isOpen &&
+        <MoreVertIcon fontSize='small' onClick={handleToggle} />
+        {isOpen && (
           <CustomSelect>
             <ul>
-              <li onClick={() => navigate(`/playlist/select/update`, {
-                      state : {
-                        playlistSeq: `${playlistSeq}`,
-                        playlistMusic: playlistMusic
-                      }
-                    })}
-                      >
+              <li
+                onClick={() =>
+                  navigate(`/playlist/select/update`, {
+                    state: {
+                      playlistSeq: `${playlistSeq}`,
+                      playlistMusic: playlistMusic,
+                    },
+                  })
+                }
+              >
                 곡 추가하기
               </li>
-              <li onClick={() => navigate(`/playlist/edit/${playlistSeq}`)}>
-                수정하기
-              </li>
-              <li onClick={confirmDelete}>
-                삭제하기
-              </li>
+              <li onClick={() => navigate(`/playlist/edit/${playlistSeq}`)}>수정하기</li>
+              <li onClick={confirmDelete}>삭제하기</li>
             </ul>
           </CustomSelect>
-        }
+        )}
       </SelectSection>
     </MoreDiv>
-  )
-}
+  );
+};
 
 const StyleWrapper = styled(Wrapper)`
-  ${({theme, url}) => css`
+  ${({ theme, url }) => css`
     background-image: linear-gradient(to bottom left, rgba(0, 0, 0, 0.8), ${theme.palette.darkAlt} 70%), url(${url});
     background-size: cover;
   `}
-`
+`;
 
 const InfoContent = styled.div`
   height: 350px;
@@ -276,23 +236,24 @@ const InfoContent = styled.div`
   display: grid;
   grid-template-columns: 300px 700px;
   justify-content: start;
-`
+`;
 
 const PlaylistCover = styled.div`
   width: 300px;
   height: 300px;
-  background-color: ${({theme, coverImage}) => coverImage || theme.palette.dark};
+  background-color: ${({ theme, coverImage }) => coverImage || theme.palette.dark};
   border-radius: 5px;
   display: flex;
   justify-content: center;
   align-items: center;
 
-  ${({coverImage}) => 
-    coverImage && `
+  ${({ coverImage }) =>
+    coverImage &&
+    `
     background-image: url(${coverImage});
     background-size: cover;
   `}
-` 
+`;
 
 const RightContent = styled.div`
   width: 830px;
@@ -302,28 +263,29 @@ const RightContent = styled.div`
   padding: 5px 10px;
   position: relative;
   margin-left: 20px;
-`
+`;
 
 const Top = styled.div`
   flex-direction: column;
   flex-grow: 4;
   align-items: start;
-`
+`;
 
 const Bottom = styled.div`
   flex-grow: 1;
   justify-content: space-between;
   align-items: end;
-`
+`;
 
 const StylePlayCircleFilledRoundedIcon = styled(PlayCircleFilledRoundedIcon)`
-  
   :hover {
     transform: scale(1.1);
     cursor: pointer;
   }
 
-  ${({disabled}) => disabled && `
+  ${({ disabled }) =>
+    disabled &&
+    `
     color: gray;
     
     :hover {
@@ -331,7 +293,7 @@ const StylePlayCircleFilledRoundedIcon = styled(PlayCircleFilledRoundedIcon)`
       cursor: default;
     }
   `}
-`
+`;
 
 const MoreDiv = styled.div`
   width: 30px;
@@ -341,7 +303,7 @@ const MoreDiv = styled.div`
   cursor: pointer;
   position: relative;
   margin-right: 10px;
-`
+`;
 
 const CustomSelect = styled.div`
   position: absolute;
@@ -350,8 +312,8 @@ const CustomSelect = styled.div`
   border: 1px solid pink;
   width: 100px;
   border-radius: 10px;
-  border: 2px solid ${({theme}) => theme.palette.secondary};
-  background-color: ${({theme}) => theme.palette.dark};
+  border: 2px solid ${({ theme }) => theme.palette.secondary};
+  background-color: ${({ theme }) => theme.palette.dark};
   height: 75px;
   font-size: 14px;
 
@@ -360,9 +322,9 @@ const CustomSelect = styled.div`
   }
 
   li:hover {
-    background-color: ${({theme}) => theme.palette.hover};
+    background-color: ${({ theme }) => theme.palette.hover};
   }
-`
+`;
 
 const PlaylistTitle = styled.div`
   color: #fff;
@@ -370,7 +332,7 @@ const PlaylistTitle = styled.div`
   font-weight: bold;
   
   & p {
-    color: ${({theme}) => theme.palette.alt};
+    color: ${({ theme }) => theme.palette.alt};
     background-color: transparent;
     font-size: 50px;
     font-weight: 800;
@@ -382,7 +344,7 @@ const PlaylistTitle = styled.div`
     :focus {
       outline: none;
   }
-`
+`;
 
 const PlaylistUser = styled.div`
   justify-content: start;
@@ -393,12 +355,12 @@ const PlaylistUser = styled.div`
     border-radius: 50%;
     margin-right: 10px;
   }
-`
+`;
 
 const PrivateToggle = styled.div`
   font-weight: light;
   display: inline-block;
-`
+`;
 
 const SelectSection = styled.section`
   display: flex;
@@ -410,7 +372,7 @@ const StyleFavoriteIcon = styled(FavoriteIcon)`
   top: 10px;
   font-size: 2rem;
   color: red;
-`
+`;
 
 const StyleFavoriteBorderIcon = styled(FavoriteBorderIcon)`
   position: absolute;
@@ -418,6 +380,6 @@ const StyleFavoriteBorderIcon = styled(FavoriteBorderIcon)`
   top: 30px;
   font-size: 2rem;
   color: red;
-`
+`;
 
 export default PlaylistDetail;
